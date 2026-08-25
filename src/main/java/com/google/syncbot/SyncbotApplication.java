3c2a158d6821a9f30d04a3864380c3646712bc00
elyothacker <hackerelyot@gmail.com>
2026-08-25 18:38:55 +0430

    Add main class SyncbotApplication

---
+++ src/main/java/com/google/syncbot/SyncbotApplication.java
@@ -0,0 +1,12 @@
+package com.google.syncbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncbotApplication.class, args);
+    }
+}
